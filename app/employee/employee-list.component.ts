f5eccf861ff604e2a956f47fa3e1e44c16dbe684
Sanjay <sanjaykpani@gmail.com>
2017-02-04 16:38:39 +0530

    Added Employee List Component file

---
+++ app/employee/employee-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'employee-list',
+    templateUrl:'app/employee/employee-list.component.html'
+})
+export class EmployeeListComponent{
+
+}
