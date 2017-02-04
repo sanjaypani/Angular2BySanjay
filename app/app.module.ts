ced3fd2bc972d143dab98ac124cfa2a3979988aa
Sanjay <sanjaykpani@gmail.com>
2017-02-04 14:03:22 +0530

    Added the Main and AppComponent
    Require to BootStrap the application

---
+++ app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+import { EmployeeListComponent } from './employee/employee-list.component'
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ AppComponent, EmployeeListComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
