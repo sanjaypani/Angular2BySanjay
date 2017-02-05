c08c1f41c32146c3291c82e0d466dcfeb5026321
sanjaypani <sanjaykpani@gmail.com>
2017-02-05 21:15:41 +0530

    Added the image display related code

--- app/employee/employee-list.component.ts
+++ app/employee/employee-list.component.ts
@@ -7,21 +7,30 @@ import { Component } from '@angular/core';
 export class EmployeeListComponent{
 
     pageTitle : string = 'Employee List!';
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+    showImage : boolean = false;
     employees : any[] = [
      {
         "name": "Sanjay",
         "id": "1",
         "country": "India",
         "salary": "100",
-        "starRating": "2.2"
+        "starRating": "2.2",
+        "imageUrl" : "http://openclipart.org/image/100px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
     },
     {
         "name": "Jitu",
         "id": "2",
         "country": "Germany",
         "salary": "200",
-        "starRating": "2.4"
+        "starRating": "2.4",
+        "imageUrl": "http://openclipart.org/image/100px/svg_to_png/58471/garden_cart.png"
     }
-    ]
+    ];
+
+    toggleImage() : void {
+        this.showImage = !this.showImage;
+    }
 
 }
